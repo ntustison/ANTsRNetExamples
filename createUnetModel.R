126614534195ba615503e120e4e2ac5feaeb8f7f
Nick Tustison <ntustison@gmail.com>
2017-08-24 13:09:43 -0700

    ENH:  Add scripts and data.

---
+++ createUnetModel.R
@@ -0,0 +1,61 @@
+createUnetModel2D <- function( inputImageSize, 
+                               layers = 1:4, 
+                               lowestResolution = 32, 
+                               kernelSize = c( 3, 3 ), 
+                               poolSize = c( 2, 2 ), 
+                               strides = c( 2, 2 )
+                             )
+{
+
+if ( ! usePkg( "keras" ) )
+  {
+  stop( "Please install the keras package." )
+  }
+
+inputs <- layer_input( shape = c( inputImageSize, 1 ) )
+
+# Encoding path  
+
+encodingConvolutionLayers <- list()
+for( i in 1:length( layers ) )
+  {
+  numberOfFilters <- lowestResolution * 2 ^ ( layers[i] - 1 )
+
+  if( i == 1 )
+    {
+    conv <- inputs %>% layer_conv_2d( filters = numberOfFilters, kernel_size = kernelSize, activation = 'relu', padding = 'same' )
+    } else {
+    conv <- pool %>% layer_conv_2d( filters = numberOfFilters, kernel_size = kernelSize, activation = 'relu', padding = 'same' )
+    }
+  encodingConvolutionLayers[[i]] <- conv %>% layer_conv_2d( filters = numberOfFilters, kernel_size = kernelSize, activation = 'relu', padding = 'same' )
+  
+  if( i < length( layers ) )
+    {
+    pool <- encodingConvolutionLayers[[i]] %>% layer_max_pooling_2d( pool_size = poolSize, strides = strides )
+    }
+  }
+
+# Decoding path 
+
+outputs <- encodingConvolutionLayers[[length( layers )]]
+for( i in 2:length( layers ) )
+  {
+  numberOfFilters <- lowestResolution * 2 ^ ( length( layers ) - layers[i] )    
+  outputs <- layer_concatenate( list( outputs %>%  
+    layer_conv_2d_transpose( filters = numberOfFilters, 
+      kernel_size = kernelSize, strides = strides, activation = 'relu', padding = 'same' ),
+    encodingConvolutionLayers[[length( layers ) - i + 1]] ),
+    axis = 3
+    )
+
+  outputs %>%
+    layer_conv_2d( filters = numberOfFilters, kernel_size = kernelSize, activation = 'relu', padding = 'same'  )  
+  }
+unetModel <- keras_model( inputs = inputs, outputs = outputs )
+unetModel %>% compile( loss = 'categorical_crossentropy',
+  optimizer = optimizer_adam( lr = 0.00001 , decay = 1e-6 ),  
+  metrics = c( 'accuracy' ) )
+
+return( unetModel )
+}
+
---
+++ trainModel.R
@@ -0,0 +1,34 @@
+library( ANTsR )
+library( keras )
+
+baseDirectory <- '/Users/ntustison/Desktop/UNet/'
+dataDirectory <- paste0( baseDirectory, 'Images/' )
+trainingDirectory <- paste0( dataDirectory, 'TrainingData/' )
+testingDirectory <- paste0( dataDirectory, 'TestingData/' )
+
+source( paste0( baseDirectory, 'createUnetModel.R' ) )
+
+trainingImageFiles <- list.files( path = trainingDirectory, pattern = "H1_2D", full.names = TRUE )
+trainingMaskFiles <- list.files( path = trainingDirectory, pattern = "Mask_2D", full.names = TRUE )
+
+trainingImages <- list()
+trainingMasks <- list()
+trainingImageArrays <- list()
+trainingMaskArrays <- list()
+
+for ( i in 1:length( trainingImageFiles ) )
+  {
+  trainingImages[[i]] <- antsImageRead( trainingImageFiles[i], dimension = 2 )    
+  trainingMasks[[i]] <- antsImageRead( trainingMaskFiles[i], dimension = 2 )    
+
+  trainingImageArrays[[i]] <- as.array( trainingImages[[i]] )
+  trainingMaskArrays[[i]] <- as.array( trainingMasks[[i]] )  
+  }
+
+unetModel <- createUnetModel2D( dim( trainingImageArrays[[1]] )  )
+
+
+  # track <- unetModel %>% fit( X_train, Y_train, epochs = 150, batch_size = 10,
+  #              callbacks = callback_early_stopping(patience = 2, monitor = 'acc'),
+  #              validation_split = 0.3  
+
