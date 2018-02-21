4f0fc638cb06d130e92be186c85a965df23d98fb
Nick Tustison <ntustison@gmail.com>
2018-02-21 17:03:11 -0500

    ENH:  Add generator approach.

---
+++ Examples/SsdExample/ssdBatchGenerator.R
@@ -0,0 +1,174 @@
+#' @export
+
+ssdImageBatchGenerator <- R6::R6Class( "SsdImageBatchGenerator",
+
+  public = list( 
+    
+    imageList = NULL,
+
+    labels = NULL,
+
+    initialize = function( imageList = NULL, labels = NULL )
+      {
+      if( !usePkg( "magick" ) )
+        {
+        stop( "Please install the magick package." )
+        }
+
+      if( !usePkg( "abind" ) )
+        {
+        stop( "Please install the abind package." )
+        }
+
+      if( !is.null( imageList ) )
+        {
+        self$imageList <- imageList
+        } else {
+        self$imageList <- list()
+        }
+
+      if( !is.null( labels ) )
+        {
+        self$labels <- labels
+        } else {
+        self$labels <- list()
+        }
+      },
+
+    generate = function( batchSize = 32L, anchorBoxes = NULL, 
+      variances = rep( 1.0, 4 ), equalize = FALSE, brightness = NULL, 
+      flipHorizontally = NULL, translate = NULL, scale = FALSE )    
+      {
+
+      # shuffle the data
+      sampleIndices <- sample( length( self$imageList ) )
+      self$imageList <- self$imageList[sampleIndices]
+      self$labels <- self$labels[sampleIndices]
+
+      currentPassCount <- 1L
+
+      function() 
+        {
+        # Shuffle the data after each complete pass 
+
+        if( currentPassCount >= length( self$imageList ) )
+          {
+          sampleIndices <- sample( length( self$imageList ) )
+          self$imageList <- self$imageList[sampleIndices]
+          self$labels <- self$labels[sampleIndices]
+
+          currentPassCount <- 1L
+          }
+
+        batchIndices <- currentPassCount:min( 
+          ( currentPassCount + batchSize - 1L ), length( self$imageList ) )
+        batchImages <- self$imageList[batchIndices]
+        batchX <- abind( batchImages, along = 4 )
+
+        dimBatchX <- dim( batchX )
+        nDimBatchX <- length( dimBatchX )
+
+        imageSize <- dimBatchX[1:(nDimBatchX - 1)]
+        batchSize <- dimBatchX[nDimBatchX]
+
+        batchX <- aperm( batchX, c( nDimBatchX, 1:( nDimBatchX - 1 ) ) )
+        batchY <- self$labels[batchIndices]
+
+        currentPassCount <- currentPassCount + batchSize
+
+        # Boxes format is numberOfBoxes x ( xmin, xmax, ymin, ymax )
+        translateBoxes <- function( boxes, shift )
+          {
+          boxes[, 1:2] <- boxes[, 1:2] + shift[1]
+          boxes[, 3:4] <- boxes[, 3:4] + shift[2]
+          }
+
+        # Boxes format is numberOfBoxes x ( xmin, xmax, ymin, ymax )
+        flipHorizontalBoxes <- function( boxes, imageWidth )
+          {
+          boxes[, 1:2] <- imageWidth - boxes[, 2:1] + imageWidth
+          }
+
+        # Boxes format is numberOfBoxes x ( xmin, xmax, ymin, ymax )
+        scaleBoxes <- function( boxes, scaleFactor )
+          {
+          boxes <- boxes * scaleFactor
+          }
+
+        for( i in seq_len( batchSize ) )
+          {
+          if( equalize ) 
+            {
+            tempX <- image_read( batchX[i,,,] / 255 ) %>%
+              image_equalize() %>%  
+              .[[1]] %>% as.numeric()
+            batchX[i,,,] <- tempX[,, 1:3] * 255
+            }
+
+          if( !is.null( brightness ) )  
+            {
+            brightValue <- as.integer( 
+              runif( 1, min = brightness[1], max = brightness[2] ) * 100 )
+
+            tempX <- image_read( batchX[i,,,] / 255 ) %>%
+              image_modulate( brightValue ) %>%
+              .[[1]] %>% as.numeric()
+            batchX[i,,,] <- tempX[,, 1:3] * 255
+            }
+
+          if( !is.null( flipHorizontally ) && runif( 1 ) < flipHorizontally )  
+            {
+            tempX <- image_read( batchX[i,,,] / 255 ) %>%
+              image_flop() %>%  
+              .[[1]] %>% as.numeric()
+            batchX[i,,,] <- tempX[,, 1:3] * 255
+            batchY[[i]][,2:5] <- 
+              flipHorizontalBoxes( batchY[[i]][,2:5], imageSize[1] )
+            }
+          
+          if( !is.null( translate ) && runif( 1 ) < translate[[3]] )  
+            {
+            blankImage <- image_read( array( runif( 1 ), imageSize ) ) 
+            shift <- c( 0, 0 )  
+            shift[1] <- sample( c( -1, 1 ), 1 ) * 
+              as.integer( runif( 1, translate[[1]][1], translate[[1]][2] ) )
+            shift[2] <- sample( c( -1, 1 ), 1 ) * 
+              as.integer( runif( 1, translate[[2]][1], translate[[2]][2] ) )
+            shiftStringX <- ifelse( shift[1] > 0, 
+              str_c( "+", shift[1] ), as.character( shift[1] ) )  
+            shiftStringY <- ifelse( shift[2] > 0, 
+              str_c( "+", shift[2] ), as.character( shift[2] ) )   
+            offsetString <- str_c( shiftStringX, shiftStringY )
+
+            tempImage <- image_read( batchX[i,,,] / 255 )
+            tempX <- image_composite( blankImage, tempImage, 
+              offset = offsetString ) %>%  
+              .[[1]] %>% as.numeric()
+
+            batchX[i,,,] <- tempX[,, 1:3] * 255
+            batchY[[i]][,2:5] <- translateBoxes( batchY[[i]][,2:5], shift )
+            }
+
+          if( !is.null( scale ) && runif( 1 ) < scale[3] )  
+            {
+            blankImage <- image_read( array( runif( 1 ), imageSize ) ) 
+            scaleFactor <- round( runif( 1, scale[1], scale[2] ), 2 )
+            scaleString <- str_c( scaleFactor * 100, "%" )
+            scaleGeometryString <- str_c( scaleString, "x", scaleString )
+
+            tempImage <- image_read( batchX[i,,,] / 255 ) %>%
+              image_scale( scaleGeometryString )
+            tempX <- image_composite( blankImage, tempImage ) %>%
+              .[[1]] %>% as.numeric()
+
+            batchX[i,,,] <- tempX[,, 1:3] * 255
+            batchY[[i]][, 2:5] <- scaleBoxes( batchY[[i]][, 2:5], scaleFactor )
+            }
+          }
+
+        encodedBatchY <- encodeY( batchY, anchorBoxes, imageSize, variances )  
+        return( list( batchX, encodedBatchY ) )        
+        }   
+      }
+    )
+  )
---
+++ Examples/SsdExample/trainSsd7Model2.R
@@ -0,0 +1,259 @@
+library( ANTsR )
+library( keras )
+library( ggplot2 )
+library( jpeg )
+library( reticulate )
+library( stringr )
+
+
+keras::backend()$clear_session()
+
+numberOfTrainingData <- 900
+inputImageSize <- c( 250, 250 )
+
+visuallyInspectEachImage <- FALSE
+
+baseDirectory <- './'
+dataDirectory <- paste0( baseDirectory, './lfw_faces_tagged/' )
+imageDirectory <- paste0( dataDirectory, 'Images/' )
+annotationsDirectory <- paste0( dataDirectory, 'Annotations/' )
+dataFile <- paste0( dataDirectory, 'data.csv' )
+data <- read.csv( dataFile )  
+uniqueImageFiles <- levels( as.factor( data$frame ) )
+
+modelDirectory <- paste0( baseDirectory, '../../Models/' )
+
+source( paste0( modelDirectory, 'createSsd7Model.R' ) )
+source( paste0( modelDirectory, 'ssdUtilities.R' ) )
+source( paste0( baseDirectory, 'ssdBatchGenerator.R' ) )
+
+classes <- c( "eyes", "nose", "mouth" )
+
+###
+#
+# Read in the training image data. 
+#
+cat( "Reading images...\n" )
+pb <- txtProgressBar( min = 0, max = numberOfTrainingData, style = 3 )
+
+trainingImages <- list()
+for( i in 1:numberOfTrainingData )
+  {
+  trainingImages[[i]] <- as.array( 
+    readJPEG( paste0( imageDirectory, uniqueImageFiles[i] ) ) )
+
+  if( i %% 100 == 0 )
+    {
+    gc( verbose = FALSE )
+    }
+
+  setTxtProgressBar( pb, i )
+  }
+cat( "\nDone.\n" )
+
+###
+#
+# Read in the training boxes data. 
+#
+
+groundTruthLabels <- list()
+for( i in 1:numberOfTrainingData )
+  {
+  groundTruthBoxes <- data[which( data$frame == uniqueImageFiles[i] ),]
+  image <- trainingImages[[i]]
+  groundTruthBoxes <- 
+    data.frame( groundTruthBoxes[, 6], groundTruthBoxes[, 2:5]  )
+  colnames( groundTruthBoxes ) <- c( "class_id", 'xmin', 'xmax', 'ymin', 'ymax' )
+  groundTruthLabels[[i]] <- groundTruthBoxes
+
+  if( visuallyInspectEachImage == TRUE )
+    {
+    cat( "Drawing", uniqueImageFiles[i], "\n" )
+
+    classIds <- groundTruthBoxes[, 1]
+
+    boxColors <- c()
+    boxCaptions <- c()
+    for( j in 1:length( classIds ) )
+      {
+      boxColors[j] <- rainbow( 
+        length( classes ) )[which( classes[classIds[j]] == classes )]
+      boxCaptions[j] <- classes[which( classes[classIds[j]] == classes )]
+      }  
+    drawRectangles( image, groundTruthBoxes[, 2:5], boxColors = boxColors, 
+      captions = boxCaptions )
+    readline( prompt = "Press [enter] to continue " )
+    }  
+  }  
+
+if( visuallyInspectEachImage == TRUE )
+  {
+  cat( "\n\nDone inspecting images.\n" )
+  }
+
+###
+#
+# Create the SSD model
+#
+
+ssdOutput <- createSsd7Model2D( c( inputImageSize, 3 ), 
+  numberOfClassificationLabels = length( classes ) + 1,
+  aspectRatiosPerLayer = 
+    list( c( 1.0, 2.0, 0.5, 3.0, 1.0/3.0 ),  
+          c( 1.0, 2.0, 0.5, 3.0, 1.0/3.0 ),
+          c( 1.0, 2.0, 0.5, 3.0, 1.0/3.0 ),
+          c( 1.0, 2.0, 0.5, 3.0, 1.0/3.0 )
+        )
+  )
+
+ssdModel <- ssdOutput$ssdModel 
+anchorBoxes <- ssdOutput$anchorBoxes
+
+optimizerAdam <- optimizer_adam( 
+  lr = 0.001, beta_1 = 0.9, beta_2 = 0.999, epsilon = 1e-08, decay = 5e-04 )
+
+ssdLoss <- lossSsd$new( backgroundRatio = 3L, minNumberOfBackgroundBoxes = 0L, 
+  alpha = 1.0, numberOfClassificationLabels = length( classes ) + 1 )
+
+ssdModel %>% compile( loss = ssdLoss$compute_loss, optimizer = optimizerAdam )
+
+yaml_string <- model_to_yaml( ssdModel )
+writeLines( yaml_string, paste0( baseDirectory, "ssd7Model.yaml" ) )
+json_string <- model_to_json( ssdModel )
+writeLines( json_string, paste0( baseDirectory, "ssd7Model.json" ) )
+
+###
+#
+#  Debugging:  draw all anchor boxes
+#
+
+# image <- readJPEG( trainingImageFiles[1] )
+# for( i in 1:length( anchorBoxes) )
+#   {
+#   # cat( "Drawing anchor box:", i, "\n" )
+#   # anchorBox <- anchorBoxes[[i]]
+#   # anchorBox[, 1:2] <- anchorBox[, 1:2] * ( inputImageSize[1] - 2 ) + 1
+#   # anchorBox[, 3:4] <- anchorBox[, 3:4] * ( inputImageSize[2] - 2 ) + 1
+#   # drawRectangles( image, anchorBox[,], 
+#   #   boxColors = rainbow( nrow( anchorBox[,] ) ) )
+#   # readline( prompt = "Press [enter] to continue\n" )
+#   for( j in 1:nrow( anchorBoxes[[i]] ) )
+#     {
+#     cat( "Drawing anchor box:", i, ",", j, "\n" )
+#     anchorBox <- anchorBoxes[[i]][j,]
+#     anchorBox[1:2] <- anchorBox[1:2] * ( inputImageSize[1] - 2 ) + 1
+#     anchorBox[3:4] <- anchorBox[3:4] * ( inputImageSize[2] - 2 ) + 1
+#     drawRectangles( image, anchorBox, boxColors = "red" )
+#     readline( prompt = "Press [enter] to continue\n" )
+#     }
+#   }
+
+###
+#
+#  Debugging:  visualize corresponding anchorBoxes
+#
+
+if( visuallyInspectEachImage == TRUE )
+  {
+  Y_train <- encodeY( groundTruthLabels, anchorBoxes, inputImageSize, rep( 1.0, 4 ) )
+
+  for( i in 1:numberOfTrainingData )
+    {
+    cat( "Drawing", trainingImageFiles[i], "\n" )
+    image <- trainingImages[[i]]
+
+    # Get anchor boxes  
+    singleY <- Y_train[i,,]
+    singleY <- singleY[which( rowSums( 
+      singleY[, 2:( 1 + length( classes ) )] ) > 0 ),]
+
+    numberOfClassificationLabels <- length( classes ) + 1
+    xIndices <- numberOfClassificationLabels + 5:6
+    singleY[, xIndices] <- singleY[, xIndices] * ( inputImageSize[1] - 2 ) + 1
+    yIndices <- numberOfClassificationLabels + 7:8
+    singleY[, yIndices] <- singleY[, yIndices] * ( inputImageSize[2] - 2 ) + 1
+
+    anchorClassIds <- max.col( singleY[, 1:4] ) - 1
+
+    anchorBoxColors <- c()
+    anchorBoxCaptions <- c()
+    for( j in 1:length( anchorClassIds ) )
+      {
+      anchorBoxColors[j] <- rainbow( 
+        length( classes ) )[which( classes[anchorClassIds[j]] == classes )]
+      # anchorBoxCaptions[j] <- classes[which( classes[anchorClassIds[j]] == classes )]
+      }
+
+    # Get truth boxes
+    truthLabel <- groundTruthLabels[[i]]
+    truthClassIds <- truthLabel[, 1]
+    truthColors <- c()
+    truthCaptions <- c()
+    for( j in 1:length( truthClassIds ) )
+      {
+      truthColors[j] <- rainbow( 
+        length( classes ) )[which( classes[truthClassIds[j]] == classes )]
+      truthCaptions[j] <- classes[which( classes[truthClassIds[j]] == classes )]
+      }
+
+    boxes <- rbind( singleY[, 9:12], as.matrix( truthLabel[, 2:5] ) )
+    boxColors <- c( anchorBoxColors, truthColors )
+    confidenceValues <- c( rep( 0.2, length( anchorBoxColors ) ), rep( 1.0, length( truthColors ) ) )
+
+    drawRectangles( image, boxes, boxColors = boxColors, confidenceValues = confidenceValues )
+
+    readline( prompt = "Press [enter] to continue " )
+    }
+  }  
+
+###
+#
+# Set up the training generator
+#
+batchSize <- 32L
+
+# Split trainingData into "training" and "validation" componets for
+# training the model.
+sampleIndices <- sample( numberOfTrainingData )
+
+validationSplit <- round( ( 1 - 0.2 ) * numberOfTrainingData )
+trainingIndices <- sampleIndices[1:validationSplit]
+validationIndices <- sampleIndices[( validationSplit + 1 ):numberOfTrainingData]
+
+trainingData <- ssdImageBatchGenerator$new( 
+  imageList = trainingImages[trainingIndices], 
+  labels = groundTruthLabels[trainingIndices] )
+
+trainingDataGenerator <- trainingData$generate( batchSize = batchSize,
+  anchorBoxes = anchorBoxes, variances = rep( 1.0, 4 ), equalize = FALSE,
+  brightness = c( 0.5, 2, 0.5 ), flipHorizontally = 0.5, 
+  translate = list( c( 5, 50 ), c( 3, 30 ), 0.5 ), 
+  scale = c( 0.75, 1.3, 0.5 ) )
+
+validationData <- ssdImageBatchGenerator$new( 
+  imageList = trainingImages[validationIndices], 
+  labels = groundTruthLabels[validationIndices] )
+
+validationDataGenerator <- validationData$generate( batchSize = batchSize,
+  anchorBoxes = anchorBoxes, variances = rep( 1.0, 4 ), equalize = FALSE,
+  brightness = c( 0.5, 2, 0.5 ), flipHorizontally = 0.5, 
+  translate = list( c( 5, 50 ), c( 3, 30 ), 0.5 ), 
+  scale = c( 0.75, 1.3, 0.5 ) )
+
+###
+#
+# Run training
+#
+
+track <- ssdModel$fit_generator( 
+  generator = reticulate::py_iterator( trainingDataGenerator ), 
+  steps_per_epoch = ceiling( length( trainingIndices ) / batchSize ),
+  epochs = 100,
+  validation_data = reticulate::py_iterator( validationDataGenerator ),
+  validation_steps = ceiling( length( validationIndices ) / batchSize ),
+  callbacks = list( 
+    callback_model_checkpoint( paste0( baseDirectory, "ssd7Weights.h5" ), 
+      monitor = 'val_loss', save_best_only = TRUE )
+                  # callback_early_stopping( patience = 2, monitor = 'loss' ),
+    )
+  )
