5f542eb2cea8512d8eeb1e7da09d564889b8289a
Nick Tustison <ntustison@gmail.com>
2018-05-28 09:55:42 -0700

    Update trainProtonModel2_parallel.R

--- Examples/UnetExample/trainProtonModel2_parallel.R
+++ Examples/UnetExample/trainProtonModel2_parallel.R
@@ -3,6 +3,9 @@ library( ANTsRNet )
 library( keras )
 library( tensorflow )
 
+# Parallelization example and documentation available here:
+#  https://tensorflow.rstudio.com/keras/reference/multi_gpu_model.html
+
 keras::backend()$clear_session()
 
 baseDirectory <- './'
