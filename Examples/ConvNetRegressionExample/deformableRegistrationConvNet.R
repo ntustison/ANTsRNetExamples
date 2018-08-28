f616e774890dd4694e58372bd4d5e8ecf9eae4e1
stnava <stnava@gmail.com>
2018-08-28 14:49:10 -0400

    WIP: deformableRegistrationConvNet-cv-prior-based

--- Examples/ConvNetRegressionExample/deformableRegistrationConvNet.R
+++ Examples/ConvNetRegressionExample/deformableRegistrationConvNet.R
@@ -8,40 +8,52 @@ library( keras )
 scl = 2
 nc = 4
 sm = 0.0
-leaveout = 5
-sdt = 0.15
-bst = 1
+leaveout = c( 1, 4 )  # leave out the template
+sdt = 1.5
+if ( ! exists( "bst" ) ) bst = 1.0
 txtype = "DeformationBasis"
-if ( ! exists( "myep" ) ) myep = 30 # reasonable default
+if ( ! exists( "myep" ) ) myep = 100 # reasonable default
 ref = ri( 16 ) %>% resampleImage( scl )
 if ( ! exists( "dpca") ) {
   images <- ri( "all" )
   wlist  <- list()
-  for( i in 1:length( images ) )
+  ct = 1
+  wimgs = seq_len( length( images ) )[-leaveout]
+  for( i in c(wimgs,wimgs,wimgs) )
     {
-    cat( "Processing image", i, "\n" )
-    img  = images[[i]]
-    reg = antsRegistration( ref, img, "SyN", totalSigma = 0.5 )
-    wlist[[ i ]] =  composeTransformsToField( ref, reg$fwd[1] )
-    images[[i]] <- ( iMath( reg$warpedmovout, "Normalize" ) * 255 ) %>%
+    cat( "Processing image-MI", i, "\n" )
+    noiseimage = makeImage( ref*0+1, rnorm( prod(dim(ref)), 0, 10 ) )
+    img  = antsImageClone( images[[i]] ) %>% resampleImage( scl ) + noiseimage
+    reg = antsRegistration( ref, img, "SyN", totalSigma = 0.0,
+      affSampling=sample( c( 16, 32, 48, 20, 12 ) )[1] )
+    wlist[[ ct ]] =  composeTransformsToField( ref, reg$fwd[1] )
+    images[[ct]] <- ( iMath( reg$warpedmovout, "Normalize" ) * 1 ) %>%
       resampleImage( scl )
+    ct = ct + 1
     }
 
   mskpca = ref * 0 + 1
-#  dpca = multichannelPCA( wlist, mskpca, k=10, pcaOption=50 )
-  dpca = multichannelPCA( wlist, mskpca, pcaOption='fastICA' )
+#  dpca2 = multichannelPCA( wlist, mskpca, k=5, pcaOption=25 )
+# We need this because not all of the allowable decompositions are SVD-like.
+#  dpca = multichannelPCA( wlist, mskpca, pcaOption='fastICA' )
+  dpca = multichannelPCA( wlist, mskpca, pcaOption='svd' )
+  pcaReconCoeffs = matrix( nrow = length( wlist ), ncol = ncol(dpca$pca$v)  )
+  for ( i in 1:length( wlist ) ) {
+    wvec = multichannelToVector( wlist[[i]], mskpca )
+    mdl = lm( wvec ~ 0 + dpca$pca$v )
+    pcaReconCoeffs[ i,  ] = coefficients(mdl)
+  }
+  pcaReconCoeffsMeans = colMeans( pcaReconCoeffs )
+  pcaReconCoeffsSD = apply( pcaReconCoeffs, FUN=sd, MARGIN=2 )
   }
 
 build_model <- function( input_shape, num_regressors ) {
-
   # Define model
   myact='linear'
-#  myact='relu'
-  filtSz = c( 32, 64, 64, 32, 32 )
-  filtSz = c( 16, 32, 64, max( input_shape ), 32 )
-  filtSz = c( 64, 32, 16, 32, 64, 32 )
-  dilrt = c( as.integer(2), as.integer(2) )
-  dilrt = as.integer(1)
+  #  myact='relu'
+  filtSz = c( 16, 32, 64, max( input_shape ), 64, 32 )
+  dilrt = as.integer( 1 )
+  dilrt = as.integer( 2 )
   model <- keras_model_sequential() %>%
     layer_conv_2d(filters = filtSz[1], kernel_size = c(3,3), activation = myact,
                   input_shape = input_shape, dilation_rate = dilrt ) %>%
@@ -64,8 +76,8 @@ build_model <- function( input_shape, num_regressors ) {
 
   model %>% compile(
     loss = "mse",
-#    optimizer = optimizer_rmsprop(  ),
-    optimizer = optimizer_adam( amsgrad = TRUE ),
+    optimizer = optimizer_adam( ),
+#    optimizer = optimizer_rmsprop(),
     metrics = list("mean_absolute_error")
   )
 
@@ -73,66 +85,79 @@ build_model <- function( input_shape, num_regressors ) {
 }
 
 basisw = dpca$pcaWarps
-# multiply by magic number 100 b/c learning sensitive to scaling
+# for some decompositions, we multiply by a magic number
+# b/c learning is sensitive to scaling
 for ( i in seq_len( length( basisw ) ) )
-  basisw[[i]] = basisw[[i]] * 1
+  basisw[[i]] = basisw[[i]] / 10
 numRegressors = length( basisw )
 input_shape <- c( dim( images[[1]]), images[[1]]@components )
-regressionModel <- build_model(  input_shape, numRegressors   )
-
+mymus = pcaReconCoeffsMeans
+mysds = pcaReconCoeffsSD * sdt
 mytd <- randomImageTransformParametersBatchGenerator$new(
   imageList = images,
   transformType = "DeformationBasis",
-  sdTransform = sdt,
   imageDomain = ref,
   spatialSmoothing = sm,
   numberOfCompositions = nc,
   deformationBasis = basisw,
-  deformationBasisMeans = rep( 0, numRegressors ),
-  deformationBasisSDs = rep( sdt, numRegressors ) )
+  deformationBasisMeans = mymus,
+  deformationBasisSDs = mysds )
 tdgenfun <- mytd$generate( batchSize = 10 )
 
-track <- regressionModel$fit_generator(
-  generator = reticulate::py_iterator( tdgenfun ),
-  steps_per_epoch = 5,
-  epochs = myep  )
-
-#####################
+##################### generate from a new source anatomy
+reg = antsRegistration( ref, ri( leaveout[2] ), "SyN", totalSigma = 0.0 )
+newanat = ( iMath( reg$warpedmovout, "Normalize" ) * 1 ) %>%
+  resampleImage( scl )
 mytd2 <- randomImageTransformParametersBatchGenerator$new(
-#  imageList = list( images[[ leaveout ]] ),
-  imageList = images,
+  imageList = list( newanat ),
   transformType = "DeformationBasis",
-  sdTransform = sdt,
   imageDomain = ref,
   spatialSmoothing = sm,
   numberOfCompositions = nc,
   deformationBasis = basisw,
-  deformationBasisMeans = rep( 0, numRegressors ),
-  deformationBasisSDs = rep( sdt, numRegressors ) )
+  deformationBasisMeans = mymus,
+  deformationBasisSDs = mysds )
 tdgenfun2 <- mytd2$generate( batchSize = 1 )
+
+testpop <- tdgenfun2()
+testimg = makeImage( mskpca, testpop[[1]][1,,,1] )
+plot( testimg, doCropping = F )
+
+###
+if ( ! exists( "doTrain" ) ) doTrain = TRUE else doTrain = FALSE
+if ( doTrain ) {
+  if ( ! exists( "regressionModel" ) )
+    regressionModel <- build_model(  input_shape, numRegressors   )
+  track <- regressionModel$fit_generator(
+    generator = reticulate::py_iterator( tdgenfun ),
+    steps_per_epoch = 5,
+    epochs = myep  )
+  }
+
 #####################
 # generate new data #
 #####################
 domainMask = ref * 0 + 1
 for ( it in 1:2 ) {
+  cat("*<>*<>*<>*<>*<>*<>*<>*<>*<>*<>*<>*<>*<>*<>*<>*<>*<>*<>*\n")
   testpop <- tdgenfun2()
   k = 1
   testimg = makeImage( mskpca, testpop[[1]][k,,,1] )
   plot( testimg, doCropping = F )
   t1=Sys.time()
   predictedData <- regressionModel %>% predict( testpop[[1]], verbose = 0 )
+#  predictedData = testpop[[2]] # best possible result
   t2=Sys.time()
   reg = antsRegistration( testimg, ref, 'SyN' )
   t3=Sys.time()
   mycor = cor( as.numeric(predictedData), as.numeric( testpop[[2]] ))
   print( paste("speedup:",as.numeric(t3-t2)/as.numeric(t2-t1), 'cor',mycor))
   # we are learning the mapping from the template to the target image
-  mytx  = basisWarp( basisw, predictedData * ( bst ), nc, sm )
-  learned = applyAntsrTransformToImage( mytx,  ref, testimg  )
-  cat("*<>*<>*<>*<>*<>*<>*<>*<>*<>*<>*<>*<>*<>*<>*<>*<>*<>*<>*\n")
-  print( paste( "ref2tar", antsImageMutualInformation( testimg, ref, nBins=16)) )
-  print( paste( "lrn2tar", antsImageMutualInformation( testimg, learned, nBins=16)) )
-  print( paste( "reg2tar", antsImageMutualInformation( testimg, reg$warpedmovout, nBins=16)) )
+  print(paste( "ref2tar", antsImageMutualInformation( testimg, ref, nBins=16)) )
+    mytx  = basisWarp( basisw, predictedData * ( bst ), nc, sm )
+    learned = applyAntsrTransformToImage( mytx,  ref, testimg  )
+    print(paste("lrn2tar", antsImageMutualInformation( testimg, learned, nBins=16)))
+    print( paste( "reg2tar", antsImageMutualInformation( testimg, reg$warpedmovout, nBins=16)) )
   plot( testimg, doCropping=F, alpha = 0.5  )
   plot( reg$warpedmovout, doCropping=F, alpha = 0.5  )
   plot( learned, doCropping=F, alpha = 0.5  )
