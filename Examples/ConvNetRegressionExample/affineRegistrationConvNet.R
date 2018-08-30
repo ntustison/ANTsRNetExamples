79c9e969e55648c2ff5bcf0435b4e1b1ff723239
stnava <stnava@gmail.com>
2018-08-30 17:49:16 -0400

    ENH: better priors

--- Examples/ConvNetRegressionExample/affineRegistrationConvNet.R
+++ Examples/ConvNetRegressionExample/affineRegistrationConvNet.R
@@ -4,22 +4,35 @@ library( ANTsRNet )
 library( ANTsR )
 library( abind )
 library( keras )
+
+# for plaidml
+use_implementation(implementation = c("keras"))
+use_backend(backend = 'plaidml' )
+
+
 imageIDs <- c( "r16", "r27", "r30", "r62", "r64", "r85" )
-images <- list()
+if ( ! exists( "images" ) ) {
+  images <- list()
+  priorParams = matrix( nrow = length( imageIDs ), ncol = 6 )
+}
 scl = 4
 leaveout = 4
-sdt = 0.1
-if ( ! exists( "myep" ) ) myep = 500 # reasonable default
+sdt = 10
+if ( ! exists( "myep" ) ) myep = 50 # reasonable default
 ref = ri( 16 )
-for( i in 1:length( imageIDs ) )
-  {
-  cat( "Processing image", imageIDs[i], "\n" )
-  img  = antsImageRead( getANTsRData( imageIDs[i] ) )
-  reg = antsRegistration( ref, img, "Affine" )
-  images[[i]] <- ( iMath( reg$warpedmovout, "Normalize" ) * 255 ) %>%
-    resampleImage( scl )
-  }
+if ( length( images ) <= 1 )
+  for( i in 1:length( imageIDs ) )
+    {
+    cat( "Processing image", imageIDs[i], "\n" )
+    img  = antsImageRead( getANTsRData( imageIDs[i] ) )
+    reg = antsRegistration( ref, img, "Affine" )
+    images[[i]] <- ( iMath( reg$warpedmovout, "Normalize" ) * 255 ) %>%
+      resampleImage( scl )
+    priorParams[i,] = getAntsrTransformParameters(
+      readAntsrTransform( reg$fwdtransforms[1] ) )
+    }
 ref = ri( 16 )  %>% resampleImage( scl )
+domainMask = ref * 0 + 1
 
 build_model <- function( input_shape, num_regressors ) {
 
@@ -45,7 +58,7 @@ build_model <- function( input_shape, num_regressors ) {
 
   model %>% compile(
 #    loss = "cosine_proximity",
-    loss = "mean_absolute_error",
+    loss = "mse",
 #    optimizer = optimizer_rmsprop(),
     optimizer = optimizer_adam(  amsgrad = TRUE ),
 #    optimizer = optimizer_sgd( ),
@@ -62,14 +75,32 @@ input_shape <- c( dim( images[[1]]), 1)
 regressionModel <- build_model(  input_shape, numRegressors   )
 regressionModel %>% summary()
 
+affmns = c( 1, 0, 0, 1, 0, 0 )
+affcov = cov( priorParams ) * sdt
+affcov[ 1:6,5:6 ] = 0
+affcov[ 5:6,1:6 ] = 0
+affcov[ 5,5 ] = 1e-2
+affcov[ 6,6] = 1e-2
+
+#####################
+mytd2 <- randomImageTransformParametersBatchGenerator$new(
+  imageList = list( images[[ leaveout ]] ),
+  transformType = "Affine",
+  txParamMeans = affmns,
+  txParamSDs = affcov,
+  imageDomain = ref )
+tdgenfun2 <- mytd2$generate( batchSize = 5 )
+testpop <- tdgenfun2()
+testimg = makeImage( domainMask, testpop[[1]][1,,,1] )
+plot(testimg,doCropping=F)
 
 mytd <- randomImageTransformParametersBatchGenerator$new(
   imageList = images[ -leaveout ],
   transformType = "Affine",
-  sdTransform = sdt,
+  txParamMeans = affmns,
+  txParamSDs =  affcov,
   imageDomain = ref )
 tdgenfun <- mytd$generate( batchSize = 10 )
-
 #
 track <- regressionModel$fit_generator(
   generator = reticulate::py_iterator( tdgenfun ),
@@ -77,37 +108,32 @@ track <- regressionModel$fit_generator(
   epochs = myep  )
 
 #####################
-mytd2 <- randomImageTransformParametersBatchGenerator$new(
-  imageList = list( images[[ leaveout ]] ),
-  transformType = "Affine",
-  sdTransform = sdt,
-  imageDomain = ref )
-tdgenfun2 <- mytd2$generate( batchSize = 1 )
-#####################
 # generate new data #
 #####################
 rr = readAntsrTransform( reg$fwdtransforms[1] )
-domainMask = ref * 0 + 1
 for ( it in 1:10 ) {
   testpop <- tdgenfun2()
-  k = 1
-  testimg = makeImage( domainMask, testpop[[1]][k,,,1] )
+  testimg = makeImage( domainMask, testpop[[1]][1,,,1] )
+  t1=Sys.time()
   predictedData <- regressionModel %>% predict( testpop[[1]], verbose = 0 )
+  t2=Sys.time()
+  reg = antsRegistration( ref, testimg, 'Affine', affIterations=c(10,10) )
+  t3=Sys.time()
+  print( paste("speedup:",as.numeric(t3-t2)/as.numeric(t2-t1) ))
   # we are learning the mapping away from the template so now invert the solution
   affTx = createAntsrTransform( "AffineTransform", dimension = 2 )
   setAntsrTransformFixedParameters( affTx,
     getAntsrTransformFixedParameters(rr)*(1))
-  setAntsrTransformParameters( affTx, predictedData[k,] )
-#  setAntsrTransformParameters( affTx, testpop[[2]][k,] ) # true
+  setAntsrTransformParameters( affTx, predictedData[1,] )
+  setAntsrTransformParameters( affTx, testpop[[2]][1,] ) # true
   ####
   affTxI = invertAntsrTransform( affTx )
   learned = applyAntsrTransform( affTxI,  testimg, ref )
-  reg = antsRegistration( ref, testimg, 'Affine' )
   cat("*<>*<>*<>*<>*<>*<>*<>*<>*<>*<>*<>*<>*<>*<>*<>*<>*<>*<>*\n")
-  print( paste( "ref-test", antsImageMutualInformation( ref, testimg, nBins=16) ) )
-  print( paste( "ref-lern", antsImageMutualInformation( ref, learned, nBins=16 ) ) )
-  print( paste( "ref-reg", antsImageMutualInformation( ref, reg$warpedmovout, nBins=16)) )
+  print( paste( "ref-idt", antsImageMutualInformation( ref, testimg)))
+  print( paste( "ref-reg", antsImageMutualInformation( ref, reg$warpedmovout)))
+  print( paste( "ref-lrn", antsImageMutualInformation( ref, learned)))
   plot( testimg, doCropping=F, alpha = 0.5  )
-  plot( reg$warpedmovout, doCropping=F, alpha = 0.5  )
-  plot( learned, doCropping=F, alpha = 0.5  )
+#  plot( reg$warpedmovout, doCropping=F, alpha = 0.5  )
+#  plot( learned, doCropping=F, alpha = 0.5  )
   }
