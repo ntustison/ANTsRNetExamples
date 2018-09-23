904d157a83e3c71770ad8984aab9d9e83ed84998
brian avants <stnava@gmail.com>
2018-09-23 13:32:42 -0400

    ENH: unet regression

---
+++ Examples/UnetExample/unetRegression.R
@@ -0,0 +1,86 @@
+library( ANTsRNet )
+library( ANTsR )
+library( abind )
+library( keras )
+# for plaidml
+# use_implementation(implementation = c("keras"))
+# use_backend(backend = 'plaidml' )
+##########################################
+imageIDs <- c( "r16", "r27", "r30",  "r62", 'r64' )
+imageIDs2 <- c( "r85", "r27" ) # testing
+hiSpc = c( 1, 1 )
+loSpc = c( 4, 4 )
+################################################
+images <- list()
+denoised <- list()
+for( i in 1:length( imageIDs ) )
+  {
+  cat( "Processing image", imageIDs[i], "\n" )
+  img = antsImageRead( getANTsRData( imageIDs[i] ) ) %>% iMath("Normalize")
+  loimg = resampleImage( img, loSpc ) %>% resampleImageToTarget( img )
+  images[[i]] <- list( loimg, iMath( loimg, "Laplacian" ),
+    iMath( loimg, "Grad" )  )
+  denoised[[i]] <- img
+  }
+################################################ testing data #
+images2 <- list()
+denoised2 <- list()
+for( i in 1:length( imageIDs2 ) )
+  {
+  cat( "Processing image", imageIDs2[i], "\n" )
+  img = antsImageRead( getANTsRData( imageIDs2[i] ) ) %>% iMath("Normalize")
+  loimg = resampleImage( img, loSpc ) %>% resampleImageToTarget( img )
+  images2[[i]] <- list( loimg, iMath( loimg, "Laplacian" ),
+    iMath( loimg, "Grad" )  )
+  denoised2[[i]] <- img
+  }
+###############################################################
+nChannels = length( images[[i]] )
+unetModel <- createUnetModel2D(
+    c( dim( images[[1]][[1]] ), nChannels ),
+    numberOfFiltersAtBaseLayer = 16,
+    numberOfLayers = 6,
+    dropoutRate = 0.0, strides=c(2,2),
+    numberOfClassificationLabels = 1, mode='regression' )
+
+# custom metric via ntustison
+unetModel %>% compile( loss = loss_peak_signal_to_noise_ratio_error,
+  optimizer = optimizer_adam( lr = 0.001 ),
+  metrics = c( 'mse', peak_signal_to_noise_ratio ) )
+
+# affine augmentation
+sdAff = 0.03
+mytd <- randomImageTransformBatchGenerator$new(
+  imageList = images,
+  outcomeImageList = denoised,
+  transformType = "Affine",
+  sdAffine = sdAff,
+  imageDomain = images[[1]][[1]],
+  imageDomainY = loimg,
+  toCategorical = FALSE )
+tdgenfun <- mytd$generate( batchSize = 8 )
+################################################
+track <- unetModel$fit_generator(
+  generator = reticulate::py_iterator( tdgenfun ),
+  steps_per_epoch = 4,
+  epochs = 85 )
+################################################
+mytd2 <- randomImageTransformBatchGenerator$new(
+  imageList = images2,
+  outcomeImageList = denoised2,
+  transformType = "Affine",
+  sdAffine = sdAff,
+  imageDomain = images[[1]][[1]],
+  imageDomainY = loimg,
+  toCategorical = FALSE )
+tdgenfun2 <- mytd2$generate( batchSize = 8 )
+testpop <- tdgenfun2()
+domainMaskX = loimg * 0 + 1
+domainMaskY = img * 0 + 1
+predictedData <- unetModel %>% predict( testpop[[1]], verbose = 0 )
+testimgC1 = makeImage( domainMaskX, testpop[[1]][1,,,1] )
+predimg = makeImage( domainMaskY, predictedData[1,,,1])
+# show side by side results on test data
+layout( matrix(c(1,2), 1, 2, byrow = TRUE))
+plot( iMath(testimgC1, "Normalize"), doCropping = F )
+plot( iMath(predimg, "Normalize"), doCropping = F )
